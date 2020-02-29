4a870e8e4af924a44c61dc0157ee161308b2d23c
jack <jackjun8090@163.com>
2020-02-29 19:23:28 +0800

    从轮播图加载数据开始

---
+++ components/search.vue
@@ -0,0 +1,36 @@
+<template>
+	<view id="search">
+		<navigator url="/pages/searchs/searchs">
+			<view class="content">
+				<text class="iconfont icon-sousuo"></text>
+				<text>搜索</text>
+			</view>
+		</navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+@import url("../common/style.css");
+#search{
+	background-color: #fd3150;
+	padding: 15rpx 10rpx;
+	.content{
+		color: #ccc;
+		background-color: #FFFFFF;
+		height: 30px;
+		border-radius: 4px;
+		text-align: center;
+		line-height: 30px;
+	}
+}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,51 @@
+<template>
+	<view class="content">
+		<!-- 搜索框 -->
+		<view>
+			<search></search>
+		</view>
+		<!-- 轮播图 -->
+		<view class="swiper_main">
+			<swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+				<swiper-item>
+					<view class="swiper-item uni-bg-red">A</view>
+				</swiper-item>
+				<swiper-item>
+					<view class="swiper-item uni-bg-green">B</view>
+				</swiper-item>
+				<swiper-item>
+					<view class="swiper-item uni-bg-blue">C</view>
+				</swiper-item>
+			</swiper>
+		</view>
+	</view>
+</template>
+
+<script>
+	import search from "../../components/search.vue"
+	export default {
+		data() {
+			return {
+				// 轮播图数据
+				background: [],
+				indicatorDots: true,
+				autoplay: true,
+				interval: 2000,
+				duration: 500
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		},
+		components: {
+			search
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+
+</style>
