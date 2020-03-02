b3e3ca28379e7001329187b9b511b33b1efe2f68
jack <jackjun8090@163.com>
2020-03-02 15:50:51 +0800

    完成分类页的搜索框

--- pages/category/main.vue
+++ pages/category/main.vue
@@ -1,15 +1,22 @@
 <template>
-	<view>
-		
+	<view class="category">
+		<!-- 搜索框 -->
+		<view class="searchs">
+			<search></search>
+		</view>
 	</view>
 </template>
 
 <script>
+	import search from "../../components/search.vue"
 	export default {
 		data() {
 			return {
 				
 			};
+		},
+		components: {
+			search
 		}
 	}
 </script>
